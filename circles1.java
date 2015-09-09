dba66097e6194ba76d499f83cf2118ea48aa2890
KSchaefer23 <kvnschaefer@yahoo.com>
2015-09-08 23:53:36 -0400

    C1 - Update
    C1 - Update changes

--- circles1.java
+++ circles1.java
@@ -5,7 +5,7 @@ float x,y;
 
 // Setup:  screen size, initialization. //
 void setup() {
-  size(500, 500);        // Try changing the width & height.
+  size(800, 800);        // Try changing the width & height.
   x=  width/2;
   y=  height/2;
 }
@@ -16,7 +16,8 @@ void draw() {
   ellipse(x+60, y+60, 40, 40);
   ellipse(x+90, y+90, 20, 20);
  
-  // Move them???? //--              x=  x + 2;  y=  y + 1;
+  // Move them???? //--
+  x=  x + 2;  y=  y + 1;
   // Insert BREAK here ^
   // to uncomment the above code,
 }
